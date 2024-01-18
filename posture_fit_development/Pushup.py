509a5a5f3f0fc6c2cd765ac21d09a972722576c2
yeasus <jmiahtee@gmail.com>
2024-01-18 11:39:10 +0800

    ye

--- posture_fit_development/Pushup.py
+++ posture_fit_development/Pushup.py
@@ -18,13 +18,8 @@ while cap.isOpened():
     fps = 1 / (time_current - time_previous)
     time_previous = time_current
     ret, img = cap.read() #640 x 480
-    #Determine dimensions of video - Help with creation of box in Line 43
-    width  = cap.get(3)  # float `width`
-    height = cap.get(4)  # float `height`
-    # print(width, height)   
     img = detector.findPose(img,False)
     lmList = detector.findPosition(img,False)
-    # print(lmList)
     if len(lmList) != 0:
         elbow = detector.findAngle(img, 11, 13, 15, draw=False)
         shoulder = detector.findAngle(img, 13, 11, 23, draw=False)
