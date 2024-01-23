aaf2ad52ac17a3599aef6771ab4aab32ac768132
yeasus <jmiahtee@gmail.com>
2024-01-23 22:26:21 +0800

    h

--- posture_fit_algorithm/Squads.py
+++ posture_fit_algorithm/Squads.py
@@ -1,54 +1,31 @@
-import cv2
-import time
-import posture_fit_algorithm.Detector as Detector
-cap = cv2.VideoCapture(0)
-Detector = Detector.poseDetector()
-count = 0
-direction = 0
-form = 0
-time_previous = 0
-time_current = 0
-feedback = "Fix Form"
-while cap.isOpened():
-    response, frame = cap.read()
-    frame = cv2.flip(frame,1)    
-    frame_RGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    time_current = time.time()
-    fps = 1 / (time_current - time_previous)
-    time_previous = time_current
-    img = Detector.findPose(img=frame, draw=False)
-    lmList = Detector.findPosition(frame,False)
-    if len(lmList) != 0:
-        knee = Detector.findAngle(frame,23,25,27,draw=False)
-        hip = Detector.findAngle(frame,11,23,25,draw=False)
+from posture_fit_algorithm.Detector2 import ExerciseLogic
+from posture_fit_development.Sound import SoundPlayer
+class SquadsLogic(ExerciseLogic):
+    def get_angles_and_thresholds(self, frame) -> tuple[list[int], list[int]]:
+        knee = self.findAngle(frame,23,25,27,draw=False)
+        hip = self.findAngle(frame,11,23,25,draw=False)
         # Specify the angles and thresholds for correctness
-        angles = [hip,knee]
-        thresholds = [165,165]
-        # Use the correctForm function to check and draw lines with correct color
-        is_correct_form = Detector.correctForm(frame,angles,thresholds)
-        if is_correct_form:
-            form = 1
-        if form == 1:
-            if knee > 170 and hip >= 170:
-                feedback = "Up"
-                if direction == 0:
-                    count += 0.5
-                    direction = 1
-            else:
-                feedback = "Fix Form"
-            if knee < 90 and hip > 60:
-                feedback = "Down"
-                if direction == 1:
-                    count += 0.5
-                    direction = 0
-            else:
-                feedback = "Fix Form"
-        #Squads counter and frame rate
-        cv2.rectangle(frame,(0,380),(100,480),(0,0,0),cv2.FILLED)
-        cv2.putText(frame,str(int(count)),(25,455),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),3)
-        cv2.putText(frame,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(0,0,255),3)
-    cv2.imshow('Squads counter',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+        angles = [knee,hip]
+        thresholds = [85,165]
+        return angles, thresholds
+    def process_specific_angles(self, frame) -> None:
+        knee = self.findAngle(frame,23,25,27,draw=False)
+        hip = self.findAngle(frame,11,23,25,draw=False)
+        if knee > 170 and hip >= 170:
+            self.feedback = "Up"
+            if self.direction == 0:
+                self.count += 0.5
+                self.direction = 1
+        else:
+            self.feedback = "Fix Form"
+            sp = SoundPlayer()
+            sp.play_sound()
+        if knee < 90 and hip > 60:
+            self.feedback = "Down"
+            if self.direction == 1:
+                self.count += 0.5
+                self.direction = 0
+        else:
+            self.feedback = "Fix Form"
+            sp = SoundPlayer()
+            sp.play_sound()
--- posture_fit_development/Webcam.py
+++ posture_fit_development/Webcam.py
@@ -2,23 +2,17 @@ import cv2
 from posture_fit_algorithm.Detector2 import ExerciseLogic
 from posture_fit_algorithm.Pushup import PushupLogic  # noqa: F401
 from posture_fit_algorithm.Situp import SitupLogic
+from posture_fit_algorithm.Squads import SquadsLogic
 from icecream import ic
-
 cap = cv2.VideoCapture(0)
-
 # Change to PushupLogic("Pushup") for pushups
 exercise_logic = SitupLogic("Situp")  
-
 while cap.isOpened():
     response, frame = cap.read()
     frame = cv2.flip(frame, 1)
-
     # Call the generic process_frame method from Exercise Logic
-
     exercise_logic.process_frame(frame)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
