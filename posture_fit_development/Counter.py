d6147d185f210ab0c2cf20d4d6a386852336e94b
Kami <harshimymommy@gmail.com>
2024-01-25 10:39:17 -0800

    style: 🎨 Format counter.py
    Signed-off-by: Kami <harshimymommy@gmail.com>

--- posture_fit_development/Counter.py
+++ posture_fit_development/Counter.py
@@ -5,11 +5,12 @@ detector = detector.poseDetector()
 count = 0
 while cap.isOpened():
     response, frame = cap.read()
-    frame = cv2.flip(frame,1)    
-    frame = detector.findPose(frame,False)
-    cv2.rectangle(frame,(0,380),(120,480),(0,0,0),cv2.FILLED)
-    cv2.putText(frame,str(int(count)),(25,455),cv2.FONT_HERSHEY_PLAIN, 5,(255,255,255),5)
-    cv2.imshow('Pushup counter',frame)
+    frame = cv2.flip(frame, 1)
+    frame = detector.findPose(frame, False)
+    cv2.rectangle(frame, (0, 380), (120, 480), (0, 0, 0), cv2.FILLED)
+    cv2.putText(frame, str(int(count)), (25, 455),
+                cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+    cv2.imshow('Pushup counter', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
