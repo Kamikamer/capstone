4bbf3ad66a7306760d566bc07518d0dd6d94577b
Kami <harshimymommy@gmail.com>
2024-01-27 19:16:32 -0800

    docs: 💡 New comments!
    Signed-off-by: Kami <harshimymommy@gmail.com>

--- posture_fit_development/Sound.py
+++ posture_fit_development/Sound.py
@@ -1,16 +1,39 @@
 import os
 import time
+from typing import Self
 from playsound import playsound
 class SoundPlayer:
-    def __new__(cls):
+    def __new__(cls) -> Self:
+        """
+        Create a new instance of the SoundPlayer class if it doesn't already exist.
+        
+        Returns:
+            Self: The instance of the SoundPlayer class. If it already exists, return the existing instance (Singleton concept).
+        """
         if not hasattr(cls, 'instance'):
-            cls.instance = super(SoundPlayer, cls).__new__(cls)
+            cls.instance: Self = super(SoundPlayer, cls).__new__(cls)
         return cls.instance
+        
     def __init__(self, cooldown_duration=3) -> None:
+        """
+        Initialize the Sound object.
+
+        Args:
+            cooldown_duration (int, optional): The cooldown duration in seconds. Defaults to 3.
+        """
+
         self.last_play_time = 0
-        self.cooldown_duration = cooldown_duration     
+        self.cooldown_duration = cooldown_duration
+        
     def play_sound(self, file_name=None) -> None:
+        """
+        Plays a sound if enough time has passed since the last call.
+
+        Args:
+            file_name (str, optional): The name of the sound file to be played. Defaults to None. Passes arg to sound() method.
+        """
         current_time = time.time()
+        print("=============== Sound ===============")
         print(f"Epoch: {current_time}")
         # Check if enough time has passed since the last call
         if current_time - self.last_play_time >= self.cooldown_duration:
@@ -20,13 +43,20 @@ class SoundPlayer:
             self.sound(file_name)
         else:
             print("Cooldown period active, cannot play sound yet")
+        print("=====================================")
+
     def sound(self, specific_file=None) -> None:
+        """
+        Play a sound using the playsound library.
+
+        Args:
+            specific_file (str, optional): The specific sound file to play. Defaults to None.
+        """
         print('Playing sound using playsound lib')
         path_type = "/" if os.name == 'posix' else "\\"
-        # Pathing
         current_path = os.getcwd().split(path_type)
-        print(f"Current path: {current_path}")
         current_path[0] = "C:\\" if current_path[0] == "C:" else "/"
+
         if current_path[-1] == 'posture_fit_development':
             current_path = os.path.join(*current_path[:-1], 'assets')
         elif current_path[-1] == 'posture_fit_algorithm':
@@ -35,11 +65,12 @@ class SoundPlayer:
             current_path = os.path.join(*current_path, "assets")
         else:
             current_path = os.path.join(*current_path)
-        # Play the sound
+
         if specific_file is None:
             playsound(os.path.join(current_path + rf'{path_type}note.wav'))
         else:
             playsound(os.path.join(current_path + rf'{path_type}{specific_file}.wav'))
+
 if __name__ == '__main__':
     sp = SoundPlayer()
     sp2 = SoundPlayer()
--- test/test_sound.py
+++ test/test_sound.py
@@ -3,20 +3,35 @@ import unittest
 
 
 class TestSound(unittest.TestCase):
-    # Should play a sound and not the default windows sound
+    """
+    This class is a test on whether sound player works.
+    Should play a sound and not the default windows sound
+    """
     def test_sound(self) -> None:
+        """
+        Test the sound player by playing a sound.
+        """
         sp = SoundPlayer()
         sp.play_sound()
 
     def test_sound_specific(self) -> None:
+        """
+        Test the specific sound functionality of the SoundPlayer class.
+        """
         sp = SoundPlayer()
         sp.play_sound("IF_1")
 
     def test_sound_specific2(self) -> None:
+        """
+        Test case for playing a specific sound using the SoundPlayer class.
+        """
         sp = SoundPlayer()
         sp.play_sound("IF_2")
 
     def test_sound_buffer(self) -> None:
+        """
+        Test the sound buffer functionality.
+        """
         sp = SoundPlayer()
         sp.play_sound()
         sp.play_sound("IF_1")
