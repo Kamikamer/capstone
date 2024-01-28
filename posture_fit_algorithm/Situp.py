b24571dbd501c3de45cbfcaedcba7798b6f70d06
Kami <harshimymommy@gmail.com>
2024-01-27 19:16:08 -0800

    chore: 🍻 Ignore short work
    This DOES not WORKKKKK, don't RUNNN
    
    DON'T BOTHER RUNNING IT'S BROKEN, TO BE FIXEDD, DONT PULLL PLSS
    
    Signed-off-by: Kami <harshimymommy@gmail.com>

--- posture_fit_algorithm/Situp.py
+++ posture_fit_algorithm/Situp.py
@@ -1,18 +1,24 @@
 from posture_fit_algorithm.Detector2 import ExerciseLogic
 from posture_fit_development.Sound import SoundPlayer
 class SitupLogic(ExerciseLogic):
-    def get_angles_and_thresholds(self, frame) -> tuple[list[int], list[int]]:
+    def __init__(self, exercise_name) -> None:
+        super().__init__(exercise_name=exercise_name)
+
+    def get_angles_and_thresholds(self, frame) -> tuple[list[float], list[int]]:
         knee = self.findAngle(frame,23,25,27,draw=False)
         hip = self.findAngle(frame,11,23,25,draw=False)
         ankle = self.findAngle(frame,25,27,29,draw=False)
+
         # Specify the angles and thresholds for correctness
         angles = [hip,knee,ankle]
         thresholds = [140,85,130]
         return angles, thresholds
-    def  process_specific_angles(self, frame) -> None:
+    
+    def process_specific_angles(self, frame) -> None:
         ankle = self.findAngle(frame, 25, 27, 29, draw=False)
         knee = self.findAngle(frame, 23, 25, 27, draw=False)
         hip = self.findAngle(frame, 11, 23, 25, draw=False)
+
         if ankle <= 135 and knee <= 90 and hip >= 145:
             self.feedback = "Down"
             if self.direction == 0:
