11b50051cab364919a735273f078b4bf76101200
WJ <hawenjie6@gmail.com>
2024-01-30 11:35:21 +0800

    fix: :sparkles: added squats and fixed crashes
    add the squat.py and fix the index error encountered when running index

--- posture_fit_algorithm/Squats.py
+++ posture_fit_algorithm/Squats.py
@@ -1,39 +1,34 @@
 from posture_fit_algorithm.Detector2 import ExerciseLogic
 from posture_fit_development.Sound import SoundPlayer
-class SitupLogic(ExerciseLogic):
+class SquatsLogic(ExerciseLogic):
     def __init__(self, exercise_name) -> None:
         super().__init__(exercise_name=exercise_name, sound_type="IF_1")
 
     def get_angles_and_thresholds(self, frame) -> tuple[list[float], list[int]]:
         knee = self.findAngle(frame,23,25,27,draw=False)
         hip = self.findAngle(frame,11,23,25,draw=False)
-        ankle = self.findAngle(frame,25,27,29,draw=False)
-
         # Specify the angles and thresholds for correctness
-        angles = [hip,knee,ankle]
-        thresholds = [140,85,130]
+        angles = [hip,knee]
+        thresholds = [140,85]
         return angles, thresholds
     
     def process_specific_angles(self, frame) -> None:
-        ankle = self.findAngle(frame, 25, 27, 29, draw=False)
-        knee = self.findAngle(frame, 23, 25, 27, draw=False)
-        hip = self.findAngle(frame, 11, 23, 25, draw=False)
-
-        if ankle <= 135 and knee <= 90 and hip <= 135:
-            self.feedback = "Down"
+        knee = self.findAngle(frame,23,25,27,draw=False)
+        hip = self.findAngle(frame,11,23,25,draw=False)
+        if knee >= 100 and hip >= 145:
+            self.feedback = "Up"
             if self.direction == 0:
                 self.count += 0.5
                 self.direction = 1
         else:
             self.feedback = "Fix Form"
-            # sp = SoundPlayer()
-            # sp.play_sound()
-        if ankle <= 135 and knee <= 90 and hip <= 35:
+        if knee < 100 and hip < 100:
             self.feedback = "Down"
             if self.direction == 1:
                 self.count += 0.5
                 self.direction = 0
         else:
             self.feedback = "Fix Form"
-            # sp = SoundPlayer()
-            # sp.play_sound()
+
+    def process_frame(self, frame) -> None:
+        return super().process_frame(frame=frame)
