16c8c44d62b3f2c43ede9a9dc347f945d4ec0d7f
WJ <hawenjie6@gmail.com>
2024-01-31 16:27:26 +0800

    fix: :art: some fixes
    fixed and improve some of the code

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 from typing import Literal
 import cv2
 from posture_fit_algorithm.Pushup import PushupLogic
-from posture_fit_algorithm import Squats
-from posture_fit_algorithm import Situp
+from posture_fit_algorithm.Squats import SquatsLogic
+from posture_fit_algorithm.Situp import SitupLogic
 # from posture_fit_algorithm.Detector2 import ExerciseLogic
 from posture_fit_development.Sound import SoundPlayer
 # from posture_fit_algorithm.Squads import SquadsLogic
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     # root.mainloop()
 
     cap = cv2.VideoCapture(0)
-    exercise_logic = PushupLogic('Pushup')
+    exercise_logic = SitupLogic('Situp')
     while cap.isOpened():
         response, frame = cap.read()
         frame = cv2.flip(frame, 1)
--- posture_fit_algorithm/Pushup.py
+++ posture_fit_algorithm/Pushup.py
@@ -3,6 +3,7 @@ from posture_fit_development.Sound import SoundPlayer
 class PushupLogic(ExerciseLogic):
     def __init__(self, exercise_name) -> None:
         super().__init__(exercise_name=exercise_name, sound_type="IF_1")
+        self.prev_form_correct = True
 
     def get_angles_and_thresholds(self, frame) -> tuple[list[float], list[int]]:
         elbow = self.findAngle(frame, 11, 13, 15, draw=False)
@@ -10,7 +11,7 @@ class PushupLogic(ExerciseLogic):
         hip = self.findAngle(frame, 11, 23, 25, draw=False)
         # Specify the angles and thresholds for correctness
         angles = [elbow, shoulder, hip]
-        thresholds = [155,35,155]
+        thresholds = [180,90,180]
         return angles, thresholds
     
     def process_specific_angles(self, frame) -> None:
@@ -22,16 +23,11 @@ class PushupLogic(ExerciseLogic):
             if self.direction == 0:
                 self.count += 0.5
                 self.direction = 1
-        else:
-            self.feedback = "Fix Form"
-            # self.sp.play_sound("IF_2")
         if elbow >= 160 and shoulder >= 40 and hip >= 160:
             self.feedback = "Down"
             if self.direction == 1:
                 self.count += 0.5
                 self.direction = 0
-        else:
-            self.feedback = "Fix Form"
 
     def process_frame(self, frame) -> None:
         return super().process_frame(frame=frame)
