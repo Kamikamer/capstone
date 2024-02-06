9430f006b50536bcb7fd0697918ebb8c21a90183
WJ <hawenjie6@gmail.com>
2024-02-06 14:29:44 +0800

    feat(frontend): :sparkles: Increased sizing of webcam app
    May break at different webcam devices.

--- index.py
+++ index.py
@@ -18,36 +18,14 @@ from tkinter import messagebox
 time_previous = 0
 time_current = 0
 
-# def push_up_counter():
-#     cap = cv2.VideoCapture(0)
-#     exercise_logic = Pushup.PushupLogic("Pushup")
-#     while cap.isOpened():
-#         _, frame = cap.read()
-#         frame = cv2.flip(frame, 1)
-#         exercise_logic.process_frame(frame)
-#     cap.release()
-#     cv2.destroyAllWindows()
-
-# def start_push_up_counter():
-#     messagebox.showinfo("Pushup Counter", "Pushup Counter will start. Press 'q' to exit.")
-#     push_up_counter()
-
 if __name__ == "__main__":
-    # root = tk.Tk()
-    # root.title("Pushup Counter")
-
-    # start_button = tk.Button(root, text="Start Pushup Counter", command=start_push_up_counter)
-    # start_button = tk.Button(root, text="Start Situps Counter", command=start_push_up_counter)
-    # # start_button = tk.Button(root, text="Start Pushup Counter", command=start_push_up_counter)
-    # start_button.pack(pady=20)
-
-    # root.mainloop()
-
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
+    
     exercise_logic = PushupLogic("Pushup")
     while cap.isOpened():
         response, frame = cap.read()
-        frame = cv2.flip(frame, 1)
         exercise_logic.process_frame(frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
